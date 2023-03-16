16109d6f0c41ca3bb91e106c9625d742e2ab4c18
vsvale <viniciusdvale@gmail.com>
2023-03-16 19:32:24 -0300

    days ago not work

--- dags/dynamic_dag.py
+++ dags/dynamic_dag.py
@@ -3,7 +3,7 @@ from airflow.utils.dates import days_ago
 from airflow import DAG
 from airflow.decorators import task
 
-with DAG(dag_id="example_dynamic_task_mapping", start_date=days_ago(1)) as dag:
+with DAG(dag_id="example_dynamic_task_mapping", start_date=datetime(2023, 3, 15)) as dag:
 
     @task
     def add_one(x: int):
