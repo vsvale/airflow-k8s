025169ee1432d656a8fde4eac4de2c665da3da13
vsvale <viniciusdvale@gmail.com>
2023-03-16 19:10:13 -0300

    copy example

--- dags/alura_stock_k8s.py
+++ dags/alura_stock_k8s.py
@@ -24,18 +24,39 @@ description = "DAG to get stock data"
 @dag(schedule='@daily', default_args=default_args,catchup=False,
 tags=['alura','stock','yfinance','s3','k8s'],description=description)
 def alura_stock_k8s():
+#
+#    @task()
+#    def select_ticker():
+#        return ['AAPL','MSFT','GOOG','TSLA']
+#
+#    @task()
+#    def get_crypto_values(ticker):
+#        print(ticker)
+#
+#
+#    get_crypto_values.expand(ticker = select_ticker())
+#
 
-    @task()
-    def select_ticker():
-        return ['AAPL','MSFT','GOOG','TSLA']
+#        df = yfinance.Ticker(ticker).history(
+#            period="1d", 
+#            interval="1h",
+#            start=days_ago(1),
+#            end=days_ago(0),
+#            prepost=True,
+#            )
+#        print(df)
 
-    @task()
-    def get_crypto_values(ticker):
-        print(ticker)
+    @task
+    def add_one(x: int):
+        return x + 1
 
+    @task
+    def sum_it(values):
+        total = sum(values)
+        print(f"Total was {total}")
 
-    get_crypto_values.expand(ticker = select_ticker())
-
+    added_values = add_one.expand(x=[1, 2, 3])
+    sum_it(added_values)
 
 
 dag = alura_stock_k8s()
